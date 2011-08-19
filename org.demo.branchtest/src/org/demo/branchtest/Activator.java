e20813f058249f287ec5d6ffe66e5578fc057975
Henrik Lindberg <henrik.lindberg@cloudsmith.com>
2011-08-19 10:20:06 +0200

    Master change.

--- org.demo.branchtest/src/org/demo/branchtest/Activator.java
+++ org.demo.branchtest/src/org/demo/branchtest/Activator.java
@@ -5,7 +5,7 @@ import org.osgi.framework.BundleContext;
 
 /**
  * The activator class controls the plug-in life cycle.
- * This is the v1.1.x version
+ * This is the MASTER VERSION.
  */
 public class Activator extends AbstractUIPlugin {
 
